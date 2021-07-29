dad7ddc7200bec4af3fdc973322a18c07ef09e61
Igor Cardoso <iygorsilva@gmail.com>
2021-07-28 23:04:23 -0300

    Remoçao da venv

--- venv-todo/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/igor/Área de Trabalho/GitHub/todo-list-django/venv-todo/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
